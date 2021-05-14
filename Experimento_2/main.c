306afb50beeb2c42d8414c9406d5bb3594b4716e
Dina Rodríguez <68084694+Rod19566@users.noreply.github.com>
2021-05-13 20:11:23 -0600

    Add files via upload

--- Experimento_2/main.c
+++ Experimento_2/main.c
@@ -13,7 +13,15 @@
 #include "driverlib/systick.h"
 #include "driverlib/uart.h"
 
+extern void Timer0IntHandler(void);
+extern void UARTIntHandler(void);
+
 uint32_t ui32Period;
+
+unsigned char l = 'a';
+uint8_t r = 0;
+uint8_t b = 0;
+uint8_t g = 0;
 /**
  * main.c
  */
@@ -42,6 +50,22 @@ void setup(void){
          TimerEnable(TIMER0_BASE, TIMER_A);
          ////////////////////////////////////////////////////////////
 
+         //CONFIGURACION UART//////////////////////////
+         SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA); //SE HABILITA PUERTO A
+         SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0); //SE HABILITA EL UART
+         UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |UART_CONFIG_PAR_NONE));
+    //     GPIOPinConfigure(0x00000001); //RX
+       //  GPIOPinConfigure(0x00000401); //GPIO_PA1_U0TX
+         UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX1_8, UART_FIFO_RX1_8);
+         GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+         IntEnable(INT_UART0);
+         UARTEnable(UART0_BASE);
+         UARTIntEnable(UART_INT_RX , UART_INT_RT);
+
+         UARTIntRegister(UART0_BASE, UARTIntHandler);
+         IntMasterEnable();    // SE HABILITAN LAS INTERRUPCIONES GLOBALES
+
+
 
 }
 int main(void)
@@ -57,12 +81,54 @@ int main(void)
 void Timer0IntHandler(void)
 {
     TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-    if(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_2)){
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+   if(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_2)){
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0); //se apaga
     }else{
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 4);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2); //se prende
     } //Timer0IntHandler
 
 }
+
+void UARTIntHandler(void){
+
+    UARTIntClear(UART0_BASE, UART_INT_RX | UART_INT_RT); //SE LIMPIA EL UART
+  //  if (UARTCharsAvail(UART0_BASE)){
+            l = UARTCharGet(UART0_BASE);
+    //    }
+
+    switch(l){
+    case 'r':
+        if (r==0){
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
+            r = 1;
+        } else {
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1,0);
+            r = 0;
+        } //if // else
+        break;
+    case 'b':
+        if (b==0){
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+            b = 1;
+        } else {
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2,0);
+            b = 0;
+        } //if // else
+        break;
+    case 'g':
+        if (g==0){
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
+            g = 1;
+        } else {
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3,0);
+            g = 0;
+        } //if // else
+            break;
+    } //switch l
+
+
+
+
+}//void UARTIntHandler
 
 
